4ce1e2b780668ec8ed76b05cd309f943d991e149
EiChinn <eichinn77@gmail.com>
2017-01-07 16:14:55 +0800

    显式地为简单工厂添加一个私有的构造方法来防止创建简单工厂的实例对象，因为完全没有这个必要

--- src/com/eichinn/simple_factory/Factory.java
+++ src/com/eichinn/simple_factory/Factory.java
@@ -4,6 +4,9 @@ package com.eichinn.simple_factory;
  * Created by ei_chinn on 2017/1/5.
  */
 public class Factory {
+    private Factory() {//防止创建Factory的实例，因为没有必要
+    }
+
     public static Api createApi() {
         return new Impl();
     }
